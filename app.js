311d09be735eb4168c365cdacdf9d7b7011018db
inezabonte <inezabonte@gmail.com>
2020-09-16 07:57:39 +0200

    moved all the routes to the index.js file

--- app.js
+++ app.js
@@ -6,29 +6,8 @@ require("dotenv/config");
 
 app.use(bodyParser.json());
 
-//about page
-const aboutRoute = require("./routes/about");
-app.use("/about", aboutRoute);
-
-//blog page
-const blogRoute = require("./routes/blog");
-app.use("/blog", blogRoute);
-
-//contact page
-const contactRoute = require("./routes/contact");
-app.use("/contact", contactRoute);
-
-//dashboard page
-const dashboardRoute = require("./routes/dashboard");
-app.use("/dashboard", dashboardRoute);
-
-//loginpage
-const loginRoute = require("./routes/login");
-app.use("/login", loginRoute);
-
-//home route
-const homepageRoute = require("./routes/homepage");
-app.use("/", homepageRoute);
+//import the routes
+require("./routes/index")(app);
 
 // Connect to DB
 //The username and password are stored in a dotenv file for security
