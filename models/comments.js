9957556cd2f27848f8ed1827f5e8572cd614fe6f
inezabonte <inezabonte@gmail.com>
2020-09-22 23:38:03 +0200

    created schema for comments

---
+++ models/comments.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const commentsScehma = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  discussion: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("comment", commentsScehma);
