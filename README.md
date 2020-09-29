7b732b274bbdf64d2cf1da817cfdfbcb98ed62b8
inezabonte <inezabonte@gmail.com>
2020-09-29 15:43:15 +0200

    changed folders to deploy on heroku

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 
 [![Build Status](https://travis-ci.com/inezabonte/express-app.svg?branch=testing)](https://travis-ci.com/inezabonte/express-app)
 
-[![Coverage Status](https://coveralls.io/repos/github/inezabonte/express-app/badge.svg)](https://coveralls.io/github/inezabonte/express-app)
-
 Building an API using Node.js and Express for my [Project](https://github.com/inezabonte/Bonte-MyBrand)
---
+++ test/contact.js
@@ -0,0 +1,62 @@
+import chai, { expect } from "chai";
+import server from "../app";
+import chaiHttp from "chai-http";
+import Contacts from "../models/contact";
+
+chai.use(chaiHttp);
+
+describe("The Contact Route", () => {
+  describe("/contact", () => {
+    //when visiting the contact page
+    it("Should return the welcome message", (done) => {
+      chai
+        .request(server)
+        .get("/contact")
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+
+    //should send a message
+    it("should send message Successfully", (done) => {
+      const message = {
+        _id: "",
+        name: "James Corden",
+        email: "jamescorden@gmail.com",
+        message: "Hi can we meet",
+      };
+      chai
+        .request(server)
+        .post("/contact")
+        .send(message)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.body.name).to.equal("James Corden");
+          expect(res.body.email).to.equal("jamescorden@gmail.com");
+          expect(res.body.message).to.equal("Hi can we meet");
+          done();
+        });
+    });
+
+    // it("should not send message Successfully", (done) => {
+    //   const message = {
+    //     name: "James Corden",
+    //     email: "jamescorden@gmail.com",
+    //     number: "Hi, can we meet",
+    //   };
+    //   chai
+    //     .request(server)
+    //     .post("/contact")
+    //     .send(message)
+    //     .end((err, res) => {
+    //       console.log(err);
+    //       if (err) done(err);
+    //       expect(res).status(400);
+    //       expect(res).to.be.a("Object");
+    //       done();
+    //     });
+    // });
+  });
+});
